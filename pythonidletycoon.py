1ecdefcc05ed964a40e377c0290fb546bbc17a71
Andy <andrew.giovengo@usm.edu>
2019-06-10 15:06:16 -0400

    Finished 300 Level Update

--- pythonidletycoon.py
+++ pythonidletycoon.py
@@ -1,29 +1,67 @@
-print("Welcome to Python Idle Tycoon!")
+import tkinter as tk
+from tkinter import ttk
+import time
 
 formatMoney = "${:0,.2f}"
 dividerLines = "----------------------------------------------------"
 
 
+class StoreTimer():
+    UpdateFreq = 100
+
+    def __init__(self, store):
+        self.Timer = store.Timer
+        self.store = store
+        self.TimerRunning = False
+        # self.StartTimer()
+
+    def StartTimer(self):
+        if self.TimerRunning is False:
+            self.TimerRunning = True
+            self.starttime = time.time()
+            root.after(StoreTimer.UpdateFreq, self.UpdateTimer)
+
+    def UpdateTimer(self):
+        elapsed = time.time() - self.starttime
+        if elapsed < self.Timer:
+            self.store.progressbar["value"] = elapsed / self.Timer * 100
+            root.after(StoreTimer.UpdateFreq, self.UpdateTimer)
+        else:
+            self.TimerRunning = False
+            self.store.progressbar["value"] = 0
+            self.store.MakeMoney()
+            if self.store.ManagerUnlocked is True:
+                self.StartTimer()
+
 class Store:
-    money = 25.00
+    money = 5.00
     day = 1
     StoreList = []
 
-    def __init__(self, storename, storeprofit, storecost):
+    def __init__(self, storename, storeprofit, storecost, timer, managercost):
 
         self.storeName = storename
         self.storeCount = 0
         self.storeProfit = storeprofit
         self.storeCost = storecost
+        self.Timer = timer
+        self.ManagerUnlocked = False
+        self.ManagerCost = managercost
+        self.TimerObject = StoreTimer(self)
+
 
     @classmethod
-    def display_game_info(cls):
-        print(dividerLines)
-        print("Day #" + str(cls.day))
-        print("Money = %s" % formatMoney.format(cls.money))
-
-        print(dividerLines)
-        print("Stores".ljust(25) + "Store Cost".ljust(15) + "Store Count")
+    def display_stores(cls):
+        Store_Label_col1 = tk.Label(root, text="Store Name")
+        Store_Label_col1.grid(row=4, column=0)
+        Store_Label_col2 = tk.Label(root, text="Progress")
+        Store_Label_col2.grid(row=4, column=1)
+        Store_Label_col3 = tk.Label(root, text="Store Cost")
+        Store_Label_col3.grid(row=4, column=2)
+        Store_Label_col4 = tk.Label(root, text="Store Count")
+        Store_Label_col4.grid(row=4, column=3)
+        Store_Label_col5 = tk.Label(root, text="Buy Store")
+        Store_Label_col5.grid(row=4, column=4)
         i = 1
         for store in cls.StoreList:
             store.display_store_info(i)
@@ -31,66 +69,70 @@ class Store:
         print(dividerLines)
 
     def display_store_info(self, i):
-        # print("%s      %d" % (self.storeName, self.storeCount))
-        storeCostStr = formatMoney.format(self.storeCost).rjust(12)
-        print(str(i) + ") " + self.storeName.ljust(20) + storeCostStr.ljust(25) + str(self.storeCount))
+        self.clickbutton = tk.Button(root, text=self.storeName, command=lambda: self.ClickStore())
+        self.clickbutton.grid(row=4 + i, column=0)
+        self.progressbar = ttk.Progressbar(root, value=0, maximum=100, orient=tk.HORIZONTAL, length=190, mode='indeterminate')
+        self.progressbar.grid(row=4 + i, column=1)
+        self.storecostlabel = tk.Label(root, text=formatMoney.format(self.storeCost))
+        self.storecostlabel.grid(row=4 + i, column=2)
+        self.storecountlabel = tk.Label(root, text=self.storeCount)
+        self.storecountlabel.grid(row=4 + i, column=3)
+        self.buybutton = tk.Button(root, text="Buy", command=lambda: self.buy_store())
+        self.buybutton.grid(row=4 + i, column=4)
+        self.managerbutton = tk.Button(root, text="Unlock Manager", command=lambda: self.UnlockManager())
+        self.managerbutton.grid(row=4 + i, column=5)
 
-    @classmethod
-    def buy_store(cls):
-        try:
-            whichstore = int(input("Which Store Do You Wish to Buy? (1-%s):" % len(Store.StoreList)))
-        except:
-            print("INVALID INPUT. Buy Aborted")
-            return
+    def buy_store(self):
+        if self.storeCost < Store.money:
+            self.storeCount += 1
 
-        if whichstore >= 1 and whichstore <= len(Store.StoreList):
-            store = Store.StoreList[whichstore - 1]
-            if store.storeCost < Store.money:
-                store.storeCount += 1
-
-                Store.money -= store.storeCost
-            else:
-                print("You don't have enough money")
+            Store.money -= self.storeCost
+            self.storecountlabel.config(text=self.storeCount)
+            Game.UpdateUI()
         else:
-            print("INCORRECT INPUT: Enter a number 1-%s" % len(Store.StoreList))
+            print("You don't have enough money")
 
-    @classmethod
-    def next_day(cls):
-        cls.day += 1
-        for store in cls.StoreList:
-            daily_profit = store.storeProfit * store.storeCount
-            cls.money += daily_profit
+    def UnlockManager(self):
+        self.ManagerUnlocked = True
 
-    @classmethod
-    def advanceWeek(cls):
-        for i in range(0, 7):
-            # print("%s - nextdaycall" % i)
-            cls.next_day()
+    def ClickStore(self):
+        self.TimerObject.StartTimer()
+
+    def MakeMoney(self):
+        daily_profit = self.storeProfit * self.storeCount
+        Store.money += daily_profit
+        Game.UpdateUI()
 
 
-# Create a New Store
-Store.StoreList.append(Store('Lemonade Stand', 1.50, 3))
-Store.StoreList.append(Store('Record Store', 5, 15))
-Store.StoreList.append(Store('Ice Cream Store', 10, 90))
+class GameManager():
+    def __init__(self):
+        self.CreateStores()
+        self.DisplayGameHeader()
+        Store.display_stores()
+
+    def CreateStores(self):
+        Store.StoreList.append(Store('Lemonade Stand', 1.50, 3, 3, 1))
+        Store.StoreList.append(Store('Record Store', 5, 15, 10, 200))
+        Store.StoreList.append(Store('Ice Cream Store', 10, 90, 30, 5000))
+
+    def DisplayGameHeader(self):
+        root.title("Python Idle Tycoon Business Game")
+
+        root.geometry("700x300")
+
+        money_label = tk.Label(root, text="Money")
+        money_label.grid(row=0, column=0)
+
+        self.money_amount_label = tk.Label(root, text=formatMoney.format(Store.money))
+        self.money_amount_label.grid(row=1, column=0)
+
+    def UpdateUI(self):
+        self.money_amount_label.config(text=Store.money)
 
 
 # Main Game
-while True:
-    # Call the Class Method to Display Game Info
-    Store.display_game_info()
 
-    # Display Store Info
+root = tk.Tk()
 
-    print("Available Options: (N)ext Day, (W)eek Advance, (B)uy Store, (Q)uit")
-    result = input("Please Enter Your Selection: ")
-    if result.upper() == "B":
-        Store.buy_store()
-    elif result.upper() == "N":
-        Store.next_day()
-    elif result.upper() == "W":
-        Store.advanceWeek()
-    elif result.upper() == "Q":
-        break
-    else:
-        print("Bad input")
-print("Thank you for playing Python Idle Tycoon")
+Game = GameManager()
+root.mainloop()
